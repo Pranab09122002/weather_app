b424bb15d5b4ed3c67c926aca61e4f1131f548e5
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2023-12-23 13:36:39 +0530

    commited

--- script.js
+++ script.js
@@ -12,15 +12,36 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const search = document.getElementById("search");
 const btn = document.getElementById("btn");
-
+const WaatherIcon = document.getElementById("weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl +city +`&appid=${apiKey}`);
     let data = await response.json();
+    console.log(data);
     document.getElementById("city").innerHTML = data.name;
-    document.getElementById("temp").innerHTML = Math.round(data.main.temp) ;
+    document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "°C" ;
     document.getElementById("humidity").innerHTML = data.main.humidity + "%";
     document.getElementById("wind").innerHTML = data.wind.speed + "Km/h";
+
+    if(data.weather[0].main =="Clouds"){
+        WaatherIcon.src = "/img/cloudy_clouds_cloud_weather_sky_icon_194252.png";
+    }
+    else if(data.weather[0].main =="Clear"){
+        WaatherIcon.src = "/img/clear-removebg-preview.png";
+    }
+    else if(data.weather[0].main =="Rain"){
+        WaatherIcon.src = "/img/rain-removebg-preview.png";
+    }
+    else if(data.weather[0].main =="Drizzle"){
+        WaatherIcon.src = "/img/drizll-removebg-preview.png";
+    }
+    else if(data.weather[0].main =="Smoke"){
+        WaatherIcon.src = "/img/mist-removebg-preview.png";
+    }
+    else if(data.weather[0].main =="Haze"){
+        WaatherIcon.src = "/img/snow-removebg-preview.png";
+    }
+    
     
 }
 
